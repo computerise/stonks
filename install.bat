6e1a7cdebc9bca7b34f1a0ac6d19b2e4a4ebfd35
computerise <tom.daniel.ashley@gmail.com>
2023-04-28 21:25:59 +0100

    Install and launch

---
+++ install.bat
@@ -0,0 +1,1 @@
+C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python311\Scripts\pip3.exe install -r requirements.txt
--- stonks/main.py
+++ stonks/main.py
@@ -1,7 +1,10 @@
 """Application entry point."""
+import time
 
 def main():
     """Launch application."""
+    print("Hello Windows.")
+    time.sleep(10)
 
 if __name__ == "__main__":
     main()
