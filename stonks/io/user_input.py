c2edbcf3875f36385eb02f470019b13efa5cbe77
computerise <tom.daniel.ashley@gmail.com>
2023-05-03 11:07:45 +0100

    remove pylint

--- stonks/io/user_input.py
+++ stonks/io/user_input.py
@@ -1,8 +1,1 @@
-from tomllib import 
-from json import read
-
-def read_toml(file):
-raise NotImplementedError
-
-def read_user_input(file):
-    raise NotImplementedError
+"""Handle user input from the Command Line Interface (CLI)."""
