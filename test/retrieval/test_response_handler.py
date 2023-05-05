0c7de90d92d7f19730500b88ac4ffb89ee7147b1
computerise <tom.daniel.ashley@gmail.com>
2023-05-05 19:09:52 +0100

    Remove test handle good response

--- test/retrieval/test_response_handler.py
+++ test/retrieval/test_response_handler.py
@@ -17,13 +17,6 @@ class TestResponseHandler(TestCase):
         response.json = lambda: json_value
         return response
 
-    @patch("json.dump", MagicMock(return_value={"mock_json"}))
-    def test_handle_good_response(self):
-        """Test handling of 200 OK response."""
-        response = self.build_mock_response("mock_url", True, 200, {"mock": "json"})
-        handle_response("mock_output", response)
-        self.assertTrue(response.ok)
-
     def test_handle_bad_response(self):
         """Test handling of a 404 bad response."""
         response = self.build_mock_response("mock_url", False, 404, {"mock": "json"})
