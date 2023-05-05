ed52854d516e596f867749de50d647b47ea33b95
computerise <tom.daniel.ashley@gmail.com>
2023-05-05 19:10:15 +0100

    Remove test handle good response

--- test/retrieval/test_response_handler.py
+++ test/retrieval/test_response_handler.py
@@ -1,7 +1,6 @@
 """Test response handler."""
 
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
 from requests.models import Response
 
 from stonks.retrieval.response_handler import handle_response
