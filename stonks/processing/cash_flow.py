118d3e0c4c70d466baf4f40f289a8959a4cb0cac
computerise <tom.daniel.ashley@gmail.com>
2023-06-11 20:29:46 +0100

    Percentage -> Rate

--- stonks/processing/cash_flow.py
+++ stonks/processing/cash_flow.py
@@ -48,33 +48,33 @@ def historical_cash_flow_increase_rate(total_cash_flows: dict[str, float]) -> fl
     """Perform historical cash flow analysis to compute the average rate of cash flow increase over all periods."""
     # Assumes cash flow statements are ordered from most-recent to least-recent.
     previous_cash_flow = None
-    percentage_increases = []
+    rate_increases = []
     # Iterate through the cash flow periods from least-recent to most-recent.
     for cash_flow_amount in list(total_cash_flows.values())[::-1]:
         if previous_cash_flow is not None:
             cash_flow_increase = cash_flow_amount - previous_cash_flow
-            percentage_increase = cash_flow_increase / abs(previous_cash_flow)
-            percentage_increases.append(percentage_increase)
+            rate_increase = cash_flow_increase / abs(previous_cash_flow)
+            rate_increases.append(rate_increase)
         # Update the cash previous cash flow
         previous_cash_flow = cash_flow_amount
-    # Take the average percentage increase.
-    cumulative_percentage_increase = sum(percentage_increases)
-    mean_cash_flow_increase_percentage = cumulative_percentage_increase / len(percentage_increases)
-    logging.info(f"Cash flow percentage increases: {percentage_increases}")
-    logging.info(f"Mean cash flow increase percentage: {mean_cash_flow_increase_percentage:,}")
-    return mean_cash_flow_increase_percentage
+    # Take the average rate increase.
+    cumulative_rate_increase = sum(rate_increases)
+    mean_cash_flow_increase_rate = cumulative_rate_increase / len(rate_increases)
+    logging.info(f"Cash flow increase rates: {rate_increases}")
+    logging.info(f"Mean cash flow increase rate: {mean_cash_flow_increase_rate:,}")
+    return mean_cash_flow_increase_rate
 
 
 def future_cash_flows(
     initial_cash_flow: float,
-    cash_flow_increase_percentage: float,
+    cash_flow_increase_rate: float,
     number_of_periods: int = 5,
 ) -> dict[str, float]:
     """Generate future cash flow estimates based on the rate of cash flow increase."""
     future_cash_flows = []
     current_cash_flow = initial_cash_flow
     for _ in range(number_of_periods):
-        current_cash_flow *= 1 + cash_flow_increase_percentage
+        current_cash_flow *= 1 + cash_flow_increase_rate
         future_cash_flows.append(current_cash_flow)
     logging.info(f"Future cash flows: {future_cash_flows}")
     return future_cash_flows
